d0eaa90b1dd5cee435a987a21a89c3fb58cdda38
ByukDor <cp1924@nyu.edu>
2020-09-01 21:18:22 +0900

    20/09/01 update

---
+++ 2920.py
@@ -0,0 +1,8 @@
+numList = list(map(int,input().split()))
+
+if numList == sorted(numList):
+    print("ascending")
+elif numList == sorted(numList, reverse = True):
+    print("descending")
+else:
+    print("mixed")
